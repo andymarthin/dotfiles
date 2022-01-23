6671d37ffa1df8eeb0a1775af7aff8c3b9fa6219
Andy Marthin <mr.andymarthin@gmail.com>
2022-01-23 20:35:57 +0700

    Fix: install google chrome condition

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -93,7 +93,7 @@ if [ ! -e "/etc/apt/sources.list.d/google-chrome.list" ]; then
   sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
   sudo apt update
 fi
-[ ! -z "$(which google-chrome)" ] && sudo apt install -y google-chrome-stable
+[ -z "$(which google-chrome)" ] && sudo apt install -y google-chrome-stable
 
 # Install Rbenv
 fancy_echo "Installing Rbenv ..."
